060ea202cabc8ca3adde6abb660b29c3c291cb72
swanjala <wanjala.sam.s@gmail.com>
2024-02-21 15:16:20 +0100

    set: static initialization of the app injector through app component

--- app/src/main/java/com/example/quizie/QuizieApp.kt
+++ app/src/main/java/com/example/quizie/QuizieApp.kt
@@ -2,6 +2,7 @@ package com.example.quizie
 
 import android.app.Activity
 import android.app.Application
+import com.example.quizie.dependencies.AppInjector
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
--- app/src/main/java/com/example/quizie/dependencies/AppInjector.kt
+++ app/src/main/java/com/example/quizie/dependencies/AppInjector.kt
@@ -1,4 +1,12 @@
 package com.example.quizie.dependencies
 
-class AppInjector {
+import com.example.quizie.QuizieApp
+
+object AppInjector {
+    fun init(application: QuizieApp) {
+        DaggerAppComponent.builder()
+            .application(application)
+            .build()
+            .inject(application)
+    }
 }
