605a52649aa264c7e91fa6a87fda9c17c82b011a
swanjala <wanjala.sam.s@gmail.com>
2024-02-21 15:14:34 +0100

    add: app component with set android injection module

---
+++ app/src/main/java/com/example/quizie/dependencies/AppComponent.kt
@@ -0,0 +1,29 @@
+package com.example.quizie.dependencies
+
+import android.app.Application
+import com.example.quizie.QuizieApp
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import javax.inject.Singleton
+
+
+@Singleton
+@Component(
+    modules = [
+        AndroidInjectionModule::class
+    ]
+)
+interface AppComponent {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): AppComponent
+
+    }
+
+    fun inject(quizieApp: QuizieApp)
+}
