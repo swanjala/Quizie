3471e4d088774ec061f34e07f47c36a3dadd55e5
swanjala <wanjala.sam.s@gmail.com>
2024-02-26 09:48:06 +0100

    Update: Data repositories

--- app/src/main/java/com/example/quizie/data/datastore/DataRepository.kt
+++ app/src/main/java/com/example/quizie/data/datastore/DataRepository.kt
@@ -1,2 +1,19 @@
 package com.example.quizie.data.datastore
 
+import com.example.quizie.data.model.Questions
+import javax.inject.Inject
+
+interface DataRepository {
+    suspend fun fetchRemoteQuestions(): List<Questions>
+}
+
+class DataRepositoryImpl @Inject constructor(
+    private val questionsRemoteDataStore: QuestionsRemoteDataStore,
+) : DataRepository {
+
+    override suspend fun fetchRemoteQuestions(): List<Questions> {
+        return questionsRemoteDataStore.fetchQuestions()
+    }
+
+}
+
